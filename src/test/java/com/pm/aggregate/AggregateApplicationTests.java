fe228d84dfca47ee9be4934d38ff80d278ece68e
MaksymPushkar <maksym.pushkar.work@gmail.com>
2024-05-25 16:18:54 +0300

    First commit

---
+++ src/test/java/com/pm/aggregate/AggregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.aggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
